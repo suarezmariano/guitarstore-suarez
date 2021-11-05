90eae7e2e0f8233cc674a9be28929fccf8ec6ebc
Mariano Suarez <marianosuarez@hotmail.com>
2021-11-04 23:14:21 -0300

    agregado de linkedin temporal

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenido a mi futura React-App
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/mariano-suarez-/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          De paso te invito a visitar mi Linkedin
         </a>
       </header>
     </div>
