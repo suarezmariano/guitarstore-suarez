52a981cba14df2aa184308934f076d704cc90c78
Mariano Suarez <marianosuarez@hotmail.com>
2021-11-13 06:55:44 -0300

    eliminado de archivos innecesarios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bienvenido a mi futura React-App
-        </p>
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/mariano-suarez-/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          De paso te invito a visitar mi Linkedin
-        </a>
-      </header>
+      
     </div>
   );
 }
