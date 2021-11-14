bb07543d68fb37fd4350e0415ae09321659955f7
suarezmariano <marianosuarez@hotmail.com>
2021-11-14 18:44:20 -0300

    terminado de navBar, favicon

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import NavBar from './components/NavBar/NavBar';
 function App() {
   return (
     <div className="App">
-      <NavBar>
-        
-      </NavBar>
-      
+      <NavBar />      
     </div>
   );
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,12 +2,17 @@ import "./NavBar.css"
 
 const NavBar = () => {
     return (
-        <nav className="container">
-            <a href="#home"><span>Guitar</span>Store</a>
-            <a href="#instruments">Instrumentos</a>
-            <a href="#services">Servicios</a>
-            <a href="#about">Sobre Nosotros</a>
-        </nav>
+        <div className="container">
+
+            <a href="#home" className="logo"><span>Guitar</span>Store</a>
+
+            <nav className="nav">
+                <a href="#instruments">Instrumentos</a>
+                <a href="#services">Servicios</a>
+                <a href="#about">Sobre Nosotros</a>
+            </nav>
+        </div>
+        
     );
 };
 
