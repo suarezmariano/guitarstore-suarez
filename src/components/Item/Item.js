b532eac5004a48ff625c3992efed968095abc547
Mariano Suarez <marianosuarez@hotmail.com>
2021-11-24 00:17:19 -0300

    cargado de items con fetch de guitarstoreapi

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,18 +1,19 @@
 import "./Item.css";
 import ItemCount from "../ItemCount/ItemCount";
 
-const Item = ({ img, model, price, description }) => (
+const Item = ({ data }) => (
   <div className="items-container">
     <div class="ui card">
       <div class="image">
-        <img src={img} alt="artículo" />
+        <img src={data.img} alt="artículo" />
       </div>
       <div class="content">
-        <div class="header">{model}</div>
+        <div class="header">{data.brand + " " + data.model}</div>
         <div class="meta">
-          <span class="date">{price}</span>
+          <span class="date">{data.description}</span>
+          <span class="date">, Año: {data.year}</span>
         </div>
-        <div class="description">{description}</div>
+        <div class="description">{data.price}</div>
         <div>
           <ItemCount stock="10" initial="0" />
         </div>
