5e0be24d432c2cce4355f85e1338e66b7dc1bc9e
Mariano Suarez <marianosuarez@hotmail.com>
2021-12-02 22:57:34 -0300

    arreglo de footer

--- src/components/Footer/Fotter.js
+++ src/components/Footer/Fotter.js
@@ -3,7 +3,38 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer-container">
-      <h2>Footer</h2>
+      <div className="box-container">
+        <div className="box">
+          <p>
+            Diseñado por Mariano Suarez | 2021 - Todos los derechos reservados.
+          </p>
+        </div>
+
+        <div className="box">
+          <a
+            href="https://portfolio-marianosuarez.web.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Portfolio
+          </a>
+          <a
+            href="https://www.linkedin.com/in/mariano-suarez-/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Linkedn
+          </a>
+          <a
+            href="https://github.com/suarezmariano"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          <a href="mailto:marianohectorsuarez@gmail.com">Enviame un Mensaje</a>
+        </div>
+      </div>
     </div>
   );
 };
