d6149ecdb67488fbd3935d047dce594ac17b66e3
suarezmariano <marianosuarez@hotmail.com>
2021-12-14 23:19:36 -0300

    agregado de quantity al widgetCart

--- src/components/WidgetCart/WidgetCart.js
+++ src/components/WidgetCart/WidgetCart.js
@@ -1,9 +1,19 @@
-import "./WidgetCart.css";
+import React, { useContext } from 'react';
+
+import './WidgetCart.css';
+import { CartContext } from '../../context/CartContext/CartContext';
 
 const WidgetCart = () => {
+  const { items } = useContext(CartContext);
+  let itemsInCart = 0;
+
+  items.map((item) => {
+    itemsInCart = itemsInCart + item.quantity;
+  });
+
   return (
-    <div className="cart">
-      <i className="cart large icon black"></i>
+    <div class="ui label">
+      <i aria-hidden="true" class="cart icon large"></i> {itemsInCart}
     </div>
   );
 };
