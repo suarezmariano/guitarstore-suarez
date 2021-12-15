a11e3030e5c5c72d1ed8283ab62ae5f963ada144
suarezmariano <marianosuarez@hotmail.com>
2021-12-15 20:35:46 -0300

    agregado de removeItem

--- src/context/CartContext/CartContext.js
+++ src/context/CartContext/CartContext.js
@@ -5,21 +5,39 @@ export const CartContext = createContext([]);
 export const CartProvider = ({ children }) => {
   const [items, setItems] = useState([]);
 
-  const addItem = (item, quantity) => {
-    setItems([
-      ...items,
-      {
-        id: item.id,
-        brand: item.brand,
-        model: item.model,
-        price: item.price,
-        quantity: quantity,
-      },
-    ]);
+  const isInCart = (id) => {
+    const found = items.find((item) => item.id === id);
+    return found;
   };
-  console.log(items);
+
+  const addItem = (item, quantity) => {
+    isInCart(item.id)
+      ? setItems(
+          items.map((prod) => {
+            if (prod.id === item.id) {
+              prod.quantity += quantity;
+            }
+            return prod;
+          })
+        )
+      : setItems([
+          ...items,
+          {
+            id: item.id,
+            brand: item.brand,
+            model: item.model,
+            price: item.price,
+            quantity: quantity,
+          },
+        ]);
+  };
+
+  const removeItem = (id) => {
+    setItems(items.filter((item) => item.id === id));
+  };
+
   return (
-    <CartContext.Provider value={{ items, addItem }}>
+    <CartContext.Provider value={{ items, addItem, removeItem }}>
       {children}
     </CartContext.Provider>
   );
--- src/views/Cart/Cart.js
+++ src/views/Cart/Cart.js
@@ -4,16 +4,18 @@ import './Cart.css';
 import { CartContext } from '../../context/CartContext/CartContext';
 
 function Cart() {
-  const { items } = useContext(CartContext);
+  const { items, removeItem } = useContext(CartContext);
 
   return (
-    <div className=".cart">
+    <div className="cart-container">
       <h1>Detalle de tu compra: </h1>
       {items.map((item) => (
         <div key={item.id}>
-          <h1>{item.id}</h1>
-          <h1>{item.price}</h1>
           <h1>{item.quantity}</h1>
+          <h1>{item.brand}</h1>
+          <h1>{item.model}</h1>
+          <h1>{item.price}</h1>
+          <h6 onClick={() => removeItem(item.id)}>Borrar Producto</h6>
         </div>
       ))}
     </div>
