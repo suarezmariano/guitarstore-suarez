31ce93925b326e9d9794d6574778134421b26fee
suarezmariano <marianosuarez@hotmail.com>
2022-01-01 22:30:35 -0300

    agregado de variables globales para firestoreconfig

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,21 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAV4L6M01xOwgAITojeI8PeeAThy22X0vQ",
-  authDomain: "guitarstore-suarez.firebaseapp.com",
-  projectId: "guitarstore-suarez",
-  storageBucket: "guitarstore-suarez.appspot.com",
-  messagingSenderId: "928165804068",
-  appId: "1:928165804068:web:f58af2ddd15a667e82c0b1"
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
